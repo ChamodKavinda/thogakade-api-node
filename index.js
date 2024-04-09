3eb9d543165e7ba5c39abda0ababb8ed64b40c0b
ChamodKavinda <kavindachamod100@gmail.com>
2024-04-10 00:05:08 +0530

    create customer crud operation

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+require('dotenv').config();
 
-const port = 3000;
+const port = process.env.PORT;
+
+const CustomerRoute=require('./route/CustomerRoute');
 
 const app = express();
 
-app.listen(port, ()=>{
-    console.log(`Thogakade Service up and running on port number ${port}`)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+mongoose.connect('mongodb://localhost:27017/thogakade').then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Thogakade Service up and running on port number ${port}`)
+    });
+}).catch(error=>{
+
 });
 
-/*app.get("/test", (req,resp)=>{
-    resp.send({'status':'Success!'})
-})*/
+app.use('/api/v1/customer',CustomerRoute);
---
+++ route/CustomerRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const customerController=require('../controller/CustomerController');
+
+const router = express.Router();
+
+router.post('/save', customerController.saveCustomer)
+router.put('/update',customerController.updateCustomer)
+router.get('/get',customerController.getCustomer)
+router.delete('/delete',customerController.deleteCustomer)
+router.get('/getAll',customerController.getAllCustomers)
+
+module.exports=router;
